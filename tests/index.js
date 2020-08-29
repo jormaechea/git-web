ecdb0d8ad3ef34575a54182e85f187e8e6ce8b31
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2020-08-29 16:28:06 -0300

    Added missing dependencies and dummy test

---
+++ tests/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const assert = require('assert');
+
+describe('Git web', () => {
+
+	context('Tests not done yet', () => {
+
+		it('Should always pass', () => {
+			assert(true);
+		});
+
+	});
+
+});
