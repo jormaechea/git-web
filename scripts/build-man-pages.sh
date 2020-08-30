6793e873c6a02af5ffbfa7cbf02e4cb337394056
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2020-08-30 19:54:30 -0300

    Fix typo in man build script

--- scripts/build-man-pages.sh
+++ scripts/build-man-pages.sh
@@ -10,9 +10,9 @@ mkdir -p $MANDSTDIR
 
 rm -f $MANDSTDIR/*
 
-npx npx marked-man ${MANSRCDIR}/git-ci.1.md > ${MANDSTDIR}/git-ci.1
-npx npx marked-man ${MANSRCDIR}/git-fork.1.md > ${MANDSTDIR}/git-fork.1
-npx npx marked-man ${MANSRCDIR}/git-issues.1.md > ${MANDSTDIR}/git-issues.1
-npx npx marked-man ${MANSRCDIR}/git-pr.1.md > ${MANDSTDIR}/git-pr.1
-npx npx marked-man ${MANSRCDIR}/git-prs.1.md > ${MANDSTDIR}/git-prs.1
-npx npx marked-man ${MANSRCDIR}/git-web.1.md > ${MANDSTDIR}/git-web.1
+npx marked-man ${MANSRCDIR}/git-ci.1.md > ${MANDSTDIR}/git-ci.1
+npx marked-man ${MANSRCDIR}/git-fork.1.md > ${MANDSTDIR}/git-fork.1
+npx marked-man ${MANSRCDIR}/git-issues.1.md > ${MANDSTDIR}/git-issues.1
+npx marked-man ${MANSRCDIR}/git-pr.1.md > ${MANDSTDIR}/git-pr.1
+npx marked-man ${MANSRCDIR}/git-prs.1.md > ${MANDSTDIR}/git-prs.1
+npx marked-man ${MANSRCDIR}/git-web.1.md > ${MANDSTDIR}/git-web.1
