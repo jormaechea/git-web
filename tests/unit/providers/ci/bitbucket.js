7d35ee1ec6db548e13c9f148fa16052ee8214eae
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2023-02-14 11:58:36 -0300

    Update new Bitbucket pipelines endpoints

--- tests/unit/providers/ci/bitbucket.js
+++ tests/unit/providers/ci/bitbucket.js
@@ -15,7 +15,7 @@ describe('Providers', () => {
 			it('Should return the CI list url', () => {
 				bitbucketRemoteUrls.forEach(remoteUrl => {
 					const remoteUrlData = gitUp(remoteUrl);
-					assert.deepStrictEqual(bitbucket.mapRemoteToUrl(remoteUrlData, {}), 'https://bitbucket.org/my-workspace/my-repo/addon/pipelines/home#!/');
+					assert.deepStrictEqual(bitbucket.mapRemoteToUrl(remoteUrlData, {}), 'https://bitbucket.org/my-workspace/my-repo/pipelines');
 				});
 			});
 
@@ -24,7 +24,7 @@ describe('Providers', () => {
 					const remoteUrlData = gitUp(remoteUrl);
 					assert.deepStrictEqual(bitbucket.mapRemoteToUrl(remoteUrlData, {
 						executionId: '455'
-					}), 'https://bitbucket.org/my-workspace/my-repo/addon/pipelines/home#!/results/455');
+					}), 'https://bitbucket.org/my-workspace/my-repo/pipelines/results/455');
 				});
 			});
 
